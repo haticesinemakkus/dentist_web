109550e6fcc9f238858bbc4a7cc2383654f5c336
Hatice Sinem Akkuş <96211726+haticesinemakkus@users.noreply.github.com>
2023-12-04 22:45:13 +0300

    Update AdminSaveRequest.php

--- app/Http/Requests/AdminSaveRequest.php
+++ app/Http/Requests/AdminSaveRequest.php
@@ -30,11 +30,9 @@ class AdminSaveRequest extends FormRequest
 
        $rules = [
             'email' => ['required','email','min:3','max:191','regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix'],
-            'title' => 'required|min:3|max:100',
             'name' => 'required|min:3|max:80',
             'surname' => 'required|min:2|max:80',
             'phone' => 'required|max:15|min:15',
-            'tc' => 'required|min:11|max:11|regex:/^[0-9]+$/u',
             'is_active' =>'in:0,1',
         ];
 
@@ -61,15 +59,8 @@ class AdminSaveRequest extends FormRequest
             'surname.required' => 'Soyad adresi girilmesi gerekiyor',
             'surname.min' => 'Soyad 2 karakterden fazla olmaldır.',
             'surname.max' => 'Soyad maximum 80 karakter olmaldır.',
-            'title.required' => 'Ünvan adresi girilmesi gerekiyor',
-            'title.min' => 'Ünvan 3 karakterden fazla olmalıdır.',
-            'title.max' => 'Ünvan 3 karakterden fazla olmalıdır.',
             'phone.min' => 'Telefon 11 karakter olmalıdır.',
             'phone.max' => 'Telefon 11 karakter olmalıdır.',
-            'tc.required' => 'Tc Kimliğin girilmesi gerekiyor',
-            'tc.min' => 'Tc Kimlik 11 karakter olmalıdır.',
-            'tc.max' => 'Tc Kimlik 11 karakter olmalıdır.',
-            'tc.regex' => 'Tc Kimlik 11 karakterli ve rakam olmalıdır.',
             'is_active' => 'Aktif veya Pasif Seçiniz.',
             'password.required' => 'Şifre girmelisiniz.',
             'password.min' => 'Şifre en az 6 karakter olabilir.',
